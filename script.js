11c833f16a8655dba8bb7058d5fd994d34cf4ac7
boobelgit config --global user.email patryk1borowski@gmail.com <patryk1borowski@gmail.com>
2022-07-12 20:37:19 +0200

    added basic book card addition

--- script.js
+++ script.js
@@ -1,38 +1,77 @@
 //declarations
 let myLibrary = [];
-let popup = document.getElementById("popup")
+const popup = document.getElementById("popup")
+let authorInput = document.querySelector("#author")
+let titleInput = document.querySelector("#title")
+let pagesInput = document.querySelector("#pages")
+let content = document.querySelector("#content")
 const closeopopupButton = document.getElementById("cancelbookbtn")
-const addBookButton = document.getElementById("addbtn")
+const addButton = document.getElementById("addbtn")
+const addBookButton = document.getElementById("addbookbtn")
 
 
 //constructors
-function Book(title, author, pages, haveread) {
-  this.title = title,
+function Book(author, title, pages, haveread) {
   this.author = author,
+  this.title = title,
   this.pages = pages,
   this.haveread = haveread
 }
 
-function addBookToLibrary(title, author, pages, haveread) {
-    return new Book(title, author, pages, haveread);
+function addBookToLibrary() {
+  let author, title, pages
+  if (authorInput.value == "") {
+    //TODO: ADD ERROR MESSAGES
+  }
+  else {
+    author = authorInput.value
+  }
+  title = titleInput.value
+  pages = pagesInput.value
+  let book = new Book(author, title, pages)
+  myLibrary.push(book)
+  closePopup()
+  displayBooks()
 }
 
-//myLibrary.push(addBookToLibrary("1", "2", "3", false))
+function displayError() {
+
+}
+
+function displayBooks() {
+  myLibrary.forEach((book) => {
+
+    let card = document.createElement('div')
+    let authorOutput = document.createElement('span')
+    let titleOutput = document.createElement('span')
+    let pagesOutput = document.createElement('span')
+
+    authorOutput.innerHTML = book.author;
+    titleOutput.innerHTML = book.title;
+    pagesOutput.innerHTML = book.pages;
+    
+    content.appendChild(card)
+    card.appendChild(authorOutput)
+    card.appendChild(titleOutput)
+    card.appendChild(pagesOutput)
+
+    card.classList.add('card')
+
+
+  });
+}
 
 function openPopup() {
   popup.classList.add("open-popup");
 }
 
-
 function closePopup() {
   popup.classList.remove("open-popup")
 }
 
+
 //runtime
-
-function pog() {
-    console.log('pog')
-}
-
-addBookButton.addEventListener("click", openPopup)
+displayBooks();
+addButton.addEventListener("click", openPopup)
 closeopopupButton.addEventListener("click", closePopup)
+addBookButton.addEventListener("click", addBookToLibrary)
