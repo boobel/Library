3c3c36c8e37f0f56a5b311a39cb0cadad82e7fb5
boobelgit config --global user.email patryk1borowski@gmail.com <patryk1borowski@gmail.com>
2022-07-13 14:48:17 +0200

    read button basic functionality

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-//declarations
 let myLibrary = [];
+let colors = ['red','green'];
+let index = 0;
 const popup = document.getElementById("popup")
 let authorInput = document.querySelector("#author")
 let titleInput = document.querySelector("#title")
@@ -9,8 +10,6 @@ const closeopopupButton = document.getElementById("cancelbookbtn")
 const addButton = document.getElementById("addbtn")
 const addBookButton = document.getElementById("addbookbtn")
 
-
-//constructors
 function Book(author, title, pages, haveread) {
   this.author = author,
   this.title = title,
@@ -47,23 +46,36 @@ function displayBooks() {
     let authorOutput = document.createElement('span');
     let titleOutput = document.createElement('span');
     let pagesOutput = document.createElement('span');
-    
+    let readBtn = document.createElement('button')
+    let delBtn = document.createElement('button')
 
     
-    authorOutput.innerHTML = book.author;
+    authorOutput.innerHTML = '"' + book.author + '"';
     titleOutput.innerHTML = book.title;
     pagesOutput.innerHTML = book.pages;
+    readBtn.innerHTML = "READ"
+    delBtn.innerHTML="DELETE"
 
 
     bookshelf.appendChild(card)
     card.appendChild(authorOutput)
     card.appendChild(titleOutput)
     card.appendChild(pagesOutput)
+    card.appendChild(readBtn)
+    card.appendChild(delBtn)
+
+
+    readBtn.classList.add('readbtn')
+    card.classList.add('card', 'new-card')
 
     
-    card.classList.add('card', 'new-card')
+    readBtn.addEventListener('click', function() {
+      readBtn.style.backgroundColor = colors[index]
+      index = index >= colors.length -1 ? 0: index +1;
+    })
   });
 }
+
 
 function openPopup() {
   popup.classList.add("open-popup");
